5c731f6bfaf4a2f0b51bc007e2e0e05f18af872a
Justin Donato <justin.donato@gmail.com>
2014-04-27 16:25:18 -0400

    bug fixes

--- mkinstall.py
+++ mkinstall.py
@@ -1,7 +1,14 @@
-appname = raw_input('What is the application name?')
-gitrepo = raw_input('Where is the git repo?')
+import sys
 
-print "Creating script for:"
+args = sys.argv[1:]
+
+if args and (len(args) == 2):
+    appname, gitrepo = args
+else:
+    appname = raw_input('What is the application name? ')
+    gitrepo = raw_input('Where is the git repo? ')
+
+print "Creating %s-install.sh for:" % appname
 print "appname: %s" % appname
 print "git repo: %s" % gitrepo
 
@@ -9,5 +16,5 @@ template = open('install.template', 'r').read()
 template = template.replace('{{ appname }}', appname)
 template = template.replace('{{ gitrepo }}', gitrepo)
 
-script = open('install.sh', 'w')
+script = open('%s-install.sh' % appname, 'w')
 script.write(template)
